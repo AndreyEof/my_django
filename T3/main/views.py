4255146f41a21f7ca849a5abf484fc1b2d1a4a0c
AndreyEof <andreyeof@gmail.com>
2022-10-31 22:17:11 +0300

    add forn create posts

--- T3/main/views.py
+++ T3/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
@@ -10,4 +11,19 @@ def about(request):
     return render(request, 'main/about.html')
 
 def create_ticket(request):
-    return render(request, 'main/create_ticket.html')
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Form is not valid'
+
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create_ticket.html', context)
